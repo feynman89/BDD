21960995248e46c52fb7fbb79977544894110917
Den <feynman89@users.noreply.github.com>
2019-05-17 09:42:00 +0700

    Тест для удаления файла

--- feature/steps/testRemoveDirAndFile.py
+++ feature/steps/testRemoveDirAndFile.py
@@ -21,3 +21,24 @@ def step_impl(context):
     global removeDirAndFile
     assert removeDirAndFile is not None
 
+
+# 2
+file_path = "D:/test/tdd/one.txt"
+@given("I have a file with a path <file_path>")
+def step_impl(context):
+    global file_path
+    my_file = open(file_path, O_CREAT)
+    close(my_file)
+
+
+@when("I call method with param <file_path> which remove file")
+def step_impl(context):
+    global removeDirAndFile, file_path
+    removeDirAndFile.removeFile(file_path)
+
+
+@then("File with a path <file_path> was remove")
+def step_impl(context):
+    global removeDirAndFile, file_path
+    assert path.exists(file_path) is False
+
