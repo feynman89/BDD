75229774382cbfe18ae0271fee3830847c678cb9
Den <feynman89@users.noreply.github.com>
2019-05-17 09:44:17 +0700

    Добавение теста для удаления папки

--- feature/steps/testRemoveDirAndFile.py
+++ feature/steps/testRemoveDirAndFile.py
@@ -42,3 +42,24 @@ def step_impl(context):
     global removeDirAndFile, file_path
     assert path.exists(file_path) is False
 
+
+# 3
+dir_path = "D:/test/tdd/hfolder"
+@given("I have a dir with a path <dir_path>")
+def step_impl(context):
+    global dir_path
+    if path.exists(dir_path) is False:
+        mkdir(dir_path)
+
+
+@when("I call method with param <dir_path> which remove dir")
+def step_impl(context):
+    global removeDirAndFile, dir_path
+    removeDirAndFile.removeDir(dir_path)
+
+
+@then("Dir with a path <dir_path> was remove")
+def step_impl(context):
+    global removeDirAndFile, dir_path
+    assert path.exists(dir_path) is False
+
